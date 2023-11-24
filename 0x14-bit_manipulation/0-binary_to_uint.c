a00d2b55285868ba399c43431eb7333565f24f99
adeyinkajames <jamesadeyinka2@gmail.com>
2023-11-23 16:31:42 -0800

    unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,22 +2,22 @@
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
- * @b: string containing the binary number
- * Return: the converted number or zero
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int j;
+	int i;
 	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[j]; j++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[j] < '0' || b[j] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[j] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
 	return (dec_val);

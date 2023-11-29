ac5e5c31e74a1ae02fc97cc3ed9b09fa62dae05f
adeyinkajames <jamesadeyinka2@gmail.com>
2023-11-29 02:11:31 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,28 +2,26 @@
 
 /**
  * create_file - Funtion that create a file.
- * filename: Name of the file created.
- * text_content: terminated string to write to the file.
- * Return: 1 on success, --1 on failure
+ * @filename: Name of the file created.
+ * @text_content: text_content.
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
-	if (!filename || !text_content)
-	{
-		return -1;
-	}
-		{
-			for (len = 0; text_content[len];)
-					len++;
-		}
+	int op, w, len = 0;
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-		w = write(fd, text_content, len);
-
-		if (fd == -1 || w == -1)
+	if (!filename)
 		return (-1);
 
-		close(fd);
+	if (!text_content)
+	text_content = "";
+
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		w = write(op, text_content, len);
+
+		if (op == -1 || w == -1)
+		return (-1);
+
+		close(op);
 		return (1);
 }

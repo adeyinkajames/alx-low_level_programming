b2ed3daafc7797d198f3f27109c18d19dce2b52e
adeyinkajames <jamesadeyinka2@gmail.com>
2023-11-28 16:20:10 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,12 +4,12 @@
  * create_file - Funtion that create a file.
  * filename: Name of the file created.
  * text_content: terminated string to write to the file.
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success, --1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
-	if (filename || text_content)
+	if (!filename || !text_content)
 	{
 		return -1;
 	}
